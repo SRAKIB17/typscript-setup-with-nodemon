62722a086b39ccc6ccebdfaa19d167719cb1f2cc
SRAKIB17 <rakibulssc5@gmail.com>
2022-10-03 23:58:15 +0600

    'gsdlfj'

---
+++ build/index.js
@@ -0,0 +1,6 @@
+"use strict";
+// define our tuple
+let ourTuple = [5, false, "534545"];
+// initialize correctly
+ourTuple = [5, false, 'Coding God was here'];
+console.log(ourTuple);
---
+++ src/index.ts
@@ -0,0 +1,7 @@
+// define our tuple
+let ourTuple: [number, boolean, string] = [5, false, "534545"]
+
+// initialize correctly
+ourTuple = [5, false, 'Coding God was here'];
+
+console.log(ourTuple)
